897c0529275cd336e69edaed30308b2e0bcfd684
rianwp <rianwp2015@gmail.com>
2022-06-22 20:26:22 +0700

    Update Carousel.vue

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -2,27 +2,37 @@
     <div class="text-center">
         <h3 class="font-poppins-bold my-4 text-start">You Might to Try</h3>
         <div class="d-flex justify-content-between my-3">
-            <a>
-                <img src="../assets/Temp1.png" alt=""/>
-            </a>
-            <a>
-                <img src="../assets/Temp2.png" alt=""/>
-            </a>
-            <a>
-                <img src="../assets/Temp3.png" alt=""/>
-            </a>
-            <a>
-                <img src="../assets/Temp4.png" alt=""/>
-            </a>
-            <a>
-                <img src="../assets/Temp5.png" alt=""/>
+            <a v-for="image in images">
+                <img :src="image.src" :alt="image.alt"/>
             </a>
         </div>
         <a href="/store" class="font-poppins-bold my-3 d-inline-block text-decoration-none link-more">More...</a>
     </div>
 </template>
 <script setup>
-
+const images = [
+    {
+        alt: "Template 1",
+        src: "src/assets/Temp1.png"
+    },
+    {
+        alt: "Template 2",
+        src: "src/assets/Temp2.png"
+    },
+    {
+        alt: "Template 3",
+        src: "src/assets/Temp3.png"
+    },
+    {
+        alt: "Template 4",
+        src: "src/assets/Temp4.png"
+    },
+    {
+        alt: "Template 5",
+        src: "src/assets/Temp5.png"
+    },
+    
+]
 </script>
 <style scoped>
 .link-more{
