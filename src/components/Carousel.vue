aa836c557f8e01a858a0f99c16897b927e562f20
rianwp <rianwp2015@gmail.com>
2022-06-26 18:55:42 +0700

    Update Carousel.vue

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="text-center">
         <h3 class="font-poppins-bold my-4 text-start">You Might to Try</h3>
-        <div class="d-flex justify-content-between my-3">
+        <div class="d-flex justify-content-between my-3 overflow-hidden">
             <a v-for="image in images">
-                <img :src="image.src" :alt="image.alt"/>
+                <img class="shadow-lg" :src="image.src" :alt="image.alt"/>
             </a>
         </div>
         <a href="/store" class="font-poppins-bold my-3 d-inline-block text-decoration-none link-more">More...</a>
@@ -13,23 +13,28 @@
 const images = [
     {
         alt: "Template 1",
-        src: "src/assets/Temp1.png"
+        src: "src/assets/Temp1.png",
+        link: ""
     },
     {
         alt: "Template 2",
-        src: "src/assets/Temp2.png"
+        src: "src/assets/Temp2.png",
+        link: ""
     },
     {
         alt: "Template 3",
-        src: "src/assets/Temp3.png"
+        src: "src/assets/Temp3.png",
+        link: ""
     },
     {
         alt: "Template 4",
-        src: "src/assets/Temp4.png"
+        src: "src/assets/Temp4.png",
+        link: ""
     },
     {
         alt: "Template 5",
-        src: "src/assets/Temp5.png"
+        src: "src/assets/Temp5.png",
+        link: ""
     },
     
 ]
