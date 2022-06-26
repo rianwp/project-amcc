1957e1c4053c3f1c91c0acbf7dd997477325e5d6
rianwp <rianwp2015@gmail.com>
2022-06-26 18:55:49 +0700

    Update IconHero.vue

--- src/components/IconHero.vue
+++ src/components/IconHero.vue
@@ -1,15 +1,28 @@
 <template>
-    <div>
-        <a :href="props.link"><slot></slot></a>
-    </div>
+   <a :href="props.link" class="link-text d-flex flex-column align-items-center">
+      <img class="icon-hero" :src="props.filename" :alt="props.alt">
+      <p class="d-none d-md-inline text-white">{{props.alt}}</p>
+    </a>     
 </template>
 <script setup>
 const props = defineProps({
   link: {
     type: String
-  }
+  },
+  filename: {
+    type: String
+  },
+  alt: {
+    type: String
+  },
 })
 </script>
 <style scoped>
-
+.link-text{
+  text-decoration: none;
+}
+.icon-hero{
+    width: 60px;
+    height: 60px;
+}
 </style>
